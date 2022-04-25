5bfe6c052adc2205d0d41823e2bcfaad3729c5f2
CARLOSAGUIRRE10 <>
2022-04-25 03:10:41 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -153,7 +153,6 @@ Pair * searchTreeMap(TreeMap * tree, void* key)
 Pair * upperBound(TreeMap * tree, void* key) 
 {
   Pair * auxPair;
-  TreeNode * aux = tree->root;
 
   auxPair = searchTreeMap(tree,key);
 
